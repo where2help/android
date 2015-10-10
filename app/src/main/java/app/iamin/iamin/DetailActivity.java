02824fd46173c92405f5b86b77930a15cf9e0242
Markus <rubey.m@gmail.com>
2015-10-10 15:18:34 +0200

    Cleaned default layout files, added list and map layout.

--- app/src/main/java/app/iamin/iamin/DetailActivity.java
+++ app/src/main/java/app/iamin/iamin/DetailActivity.java
@@ -2,6 +2,11 @@ package app.iamin.iamin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,13 +20,43 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private LinearLayout mButtonBar;
+    private Button mSubmitButton;
+    private Button mCancelButton;
+    private Button mAddButton;
+    private TextView mSubmitInfo;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Title");
+        toolbar.setNavigationIcon(R.drawable.ic_action_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mSubmitInfo = (TextView) findViewById(R.id.info);
+
+        mSubmitButton = (Button) findViewById(R.id.submit);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSubmitInfo.getVisibility() != View.VISIBLE) {
+                    mSubmitInfo.setVisibility(View.VISIBLE);
+                    mButtonBar.setVisibility(View.VISIBLE);
+                    mSubmitButton.setText("Weitersagen!");
+                } else {
+                    mSubmitButton.setText("I'm in!");
+                    mSubmitInfo.setVisibility(View.GONE);
+                    mButtonBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
