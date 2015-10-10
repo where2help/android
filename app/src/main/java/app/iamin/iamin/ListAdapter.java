6670ecd3aef15c598795eece82737c54f3b37c89
Paul Laufer <paul@laufer.at>
2015-10-10 13:12:33 +0200

    added empty list

---
+++ app/src/main/java/app/iamin/iamin/ListAdapter.java
@@ -0,0 +1,24 @@
+package app.iamin.iamin;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Paul on 10-10-2015.
+ */
+public class ListAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
