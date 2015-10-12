ec52f585cc201ee27648ca233ae7dc141a3c73b5
Markus <rubey.m@gmail.com>
2015-10-12 09:49:33 +0200

    If empty CustomRecyclerView shows a progress bar.

---
+++ app/src/main/java/app/iamin/iamin/CustomRecyclerView.java
@@ -0,0 +1,62 @@
+package app.iamin.iamin;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Markus on 12.10.15.
+ */
+public class CustomRecyclerView extends RecyclerView {
+    private View mEmptyView;
+
+    private AdapterDataObserver mDataObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            updateEmptyView();
+        }
+    };
+
+    public CustomRecyclerView(Context context) {
+        super(context);
+    }
+
+    public CustomRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * Designate a view as the empty view. When the backing adapter has no
+     * data this view will be made visible and the recycler view hidden.
+     *
+     */
+    public void setEmptyView(View emptyView) {
+        mEmptyView = emptyView;
+    }
+
+    @Override
+    public void setAdapter(RecyclerView.Adapter adapter) {
+        if (getAdapter() != null) {
+            getAdapter().unregisterAdapterDataObserver(mDataObserver);
+        }
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mDataObserver);
+        }
+        super.setAdapter(adapter);
+        updateEmptyView();
+    }
+
+    private void updateEmptyView() {
+        if (mEmptyView != null && getAdapter() != null) {
+            boolean showEmptyView = getAdapter().getItemCount() == 0;
+            mEmptyView.setVisibility(showEmptyView ? VISIBLE : GONE);
+            setVisibility(showEmptyView ? GONE : VISIBLE);
+        }
+    }
+}
--- app/src/main/java/app/iamin/iamin/MainActivity.java
+++ app/src/main/java/app/iamin/iamin/MainActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.widget.RecyclerView;
 public class MainActivity extends AppCompatActivity implements
         ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private RecyclerView mRecyclerView;
+    private CustomRecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
@@ -24,11 +24,11 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView = (CustomRecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-
+        mRecyclerView.setEmptyView(findViewById(R.id.empty_view));
         // TODO: remove
         //initializeData();
 
