9b309e2f4ac4053380a182192def751ac64a95cf
Markus <rubey.m@gmail.com>
2015-10-20 12:54:00 +0200

    Created User.java

---
+++ app/src/main/java/app/iamin/iamin/model/User.java
@@ -0,0 +1,141 @@
+package app.iamin.iamin.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ * Created by Markus on 20.10.15.
+ */
+public class User {
+
+    private int id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private boolean admin;
+
+    private boolean ngoAdmin;
+
+    private String provider;
+
+    private String uid;
+
+    private String name;
+
+    private String nickname;
+
+    private String image;
+
+    public User() {}
+
+    public User fromJSON(JSONObject obj) throws JSONException, IOException, ParseException {
+        User user = new User();
+        // TODO: Create user from JSONObject
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public boolean isNgoAdmin() {
+        return ngoAdmin;
+    }
+
+    public void setNgoAdmin(boolean ngoAdmin) {
+        this.ngoAdmin = ngoAdmin;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
