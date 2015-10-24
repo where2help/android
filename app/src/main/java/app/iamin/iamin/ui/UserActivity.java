aca013de0fdc260228aa4f29a8c685f18b76c27d
Markus <rubey.m@gmail.com>
2015-10-24 10:38:56 +0200

    Cleanup UserActivity

--- app/src/main/java/app/iamin/iamin/ui/UserActivity.java
+++ app/src/main/java/app/iamin/iamin/ui/UserActivity.java
@@ -1,6 +1,7 @@
 package app.iamin.iamin.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -15,27 +16,26 @@ import app.iamin.iamin.util.UiUtils;
 
 public class UserActivity extends AppCompatActivity {
 
-    TextView titleTextView;
-    TextView descTextView;
-
-    private User user;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        user = EndpointUtils.getUser(this);
+        User user = EndpointUtils.getUser(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("");
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        titleTextView = (TextView) findViewById(R.id.header_title);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        TextView titleTextView = (TextView) findViewById(R.id.header_title);
         titleTextView.setText(user.getFirstName());
 
-        descTextView = (TextView) findViewById(R.id.header_desc);
+        TextView descTextView = (TextView) findViewById(R.id.header_desc);
         descTextView.setText(R.string.empty_message);
     }
 
