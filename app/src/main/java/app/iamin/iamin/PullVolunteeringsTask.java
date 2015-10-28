99d87513faf688db8fe17008db816508560f6bcb
Markus <rubey.m@gmail.com>
2015-10-28 23:39:38 +0100

    Delete NeedMock

---
+++ app/src/main/java/app/iamin/iamin/PullVolunteeringsTask.java
@@ -0,0 +1,91 @@
+
+package app.iamin.iamin;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+import app.iamin.iamin.event.NeedsEvent;
+import app.iamin.iamin.model.Need;
+import app.iamin.iamin.model.User;
+import app.iamin.iamin.util.EndpointUtils;
+
+/**
+ * Created by paul on 10/10/2015.
+ */
+public class PullVolunteeringsTask extends AsyncTask<Void, Integer, Need[]> {
+    private static final String TAG = "PullVolunteeringsTask";
+
+    private Context context;
+
+    public PullVolunteeringsTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected Need[] doInBackground(Void... params) {
+
+        User user = EndpointUtils.getUser(context);
+        if (user.getEmail() == null) return null;
+
+
+        Need[] needs = null;
+        String url = EndpointUtils.getEndpoint(context);
+        Log.d(TAG, url);
+
+        Headers headers = EndpointUtils.getHeaders(context);
+/*
+        Log.e(TAG, "LOCAL Access-Token = " + headers.get("Access-Token"));
+        Log.e(TAG, "LOCAL Token-Type = " + headers.get("Token-Type"));
+        Log.e(TAG, "LOCAL Client = " + headers.get("Client"));
+        Log.e(TAG, "LOCAL Expiry = " + headers.get("Expiry"));
+        Log.e(TAG, "LOCAL Uid = " + headers.get("Uid"));
+*/
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .headers(headers)
+                    .url(url + "users/" + user.getId() + "/volunteerings")
+            .build();
+
+            Response response = client.newCall(request).execute();
+/*
+            Log.e(TAG, "Access-Token = " + response.headers().get("Access-Token"));
+            Log.e(TAG, "Token-Type = " + response.headers().get("Token-Type"));
+            Log.e(TAG, "Client = " + response.headers().get("Client"));
+            Log.e(TAG, "Expiry = " + response.headers().get("Expiry"));
+            Log.e(TAG, "Uid = " + response.headers().get("Uid"));
+*/
+            EndpointUtils.storeHeader(context, response.headers());
+
+            String result = response.body().string();
+            Log.e(TAG, "RESULT: " + result);
+/*
+            JSONArray data = new JSONObject(result).getJSONArray("data");
+
+            needs = new Need[data.length()];
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject obj = data.getJSONObject(i);
+                needs[i] = new Need().fromJSON(context, obj);
+            }
+*/
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return needs;
+    }
+
+    @Override
+    protected void onPostExecute(Need[] needs) {
+        BusProvider.getInstance().post(new NeedsEvent(needs));
+    }
+}
