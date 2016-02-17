6eeb6ddf47a845721200cc34cb86cfb9fac0ada4
Markus <rubey.m@gmail.com>
2016-02-17 10:40:43 +0100

    Make error final

--- app/src/main/java/app/iamin/iamin/data/event/ErrorEvent.java
+++ app/src/main/java/app/iamin/iamin/data/event/ErrorEvent.java
@@ -4,7 +4,7 @@ package app.iamin.iamin.data.event;
  * Created by Markus on 11.11.15.
  */
 public class ErrorEvent {
-    public String error;
+    public final String error;
 
     public ErrorEvent(String error) {
         this.error = error;
